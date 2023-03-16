70fd24a565ddb44865aa0ad630851d9234475cfe
Douglas Santana Costa <douglas.santana1777@gmail.com>
2023-03-16 17:24:05 -0300

    Commit inicial

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  return res.json("Api está ok");
+});
+const port = process.env.PORT || 3000;
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor rodando na porta ${process.env.PORT}`);
+});
