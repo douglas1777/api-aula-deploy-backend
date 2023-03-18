b279a6e42b43bc45d06d9ec22719b3218e712b29
Douglas Santana Costa <douglas.santana1777@gmail.com>
2023-03-17 22:58:59 -0300

    configurando o banco de dados

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const knex = require("./conexao");
 require("dotenv").config();
 
 const app = express();
@@ -6,6 +7,12 @@ const app = express();
 app.use(express.json());
 
 app.get("/", async (req, res) => {
+  try {
+    const carros = await knex("carros");
+    return res.status(200).json(carros);
+  } catch (error) {
+    return res.status(500).json({ mensagem: "Erro do servidor" });
+  }
   return res.json("Api deployada ");
 });
 const port = process.env.PORT || 3000;
